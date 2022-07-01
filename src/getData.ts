d447b5ee85ec9afb142a57543f629d0e46f18c37
corno <corno.astn.a@schraverus.com>
2022-07-01 21:18:49 +0200

    .

--- src/getData.ts
+++ src/getData.ts
@@ -4,7 +4,7 @@ import * as asyncLib from "pareto-async-lib"
 import * as fsLib from "pareto-filesystem-lib"
 
 import { createLeafProcessCall } from "pareto-process-lib"
-import { createLeafHTTPSCaller } from "pareto-https-lib"
+import * as https from "pareto-https-lib"
 
 export type Part = {
     isPublic: boolean
@@ -40,68 +40,59 @@ export type Depencency = {
     remote: RemoteData | null
 }
 
-function createRegistryDataGetter(
-    name: string
-): asyncAPI.IAsync<RemoteData | null> {
-
-    let data = ""
-    let hasError = false
-    return createLeafHTTPSCaller<RemoteData | null>(
-        'registry.npmjs.org',
-        `/${name}`,
-        (d) => {
-            data += d
-        },
-        (err) => {
-            console.error('https', err)
-        },
-        () => {
-            if (hasError) {
-                return null
-            }
-            const json = JSON.parse(data)
-            if (json["dist-tags"] === undefined || json["dist-tags"].latest === undefined) {
-                return {
-                    latestVersion: null,
-                    contentFingerprint: null,
-                }
-            } else {
-                const latest = json["dist-tags"].latest
-                if (json["versions"][latest] === undefined || json["versions"][latest]["content-fingerprint"] === undefined) {
-                    return {
-                        latestVersion: latest,
-                        contentFingerprint: null,
-                    }
-                } else {
-                    return {
-                        latestVersion: latest,
-                        contentFingerprint: json["versions"][latest]["content-fingerprint"],
-
-                    }
-                }
-            }
-        }
-
-    )
-}
-
 export function getData(
-    rootDir: string,
-    callback: (o: Overview) => void,
-) {
-
+    rootDir: string
+): asyncAPI.IAsync<Overview> {
     const registryCache = asyncLib.createCache(
         (key) => {
-            return createRegistryDataGetter(key)
+            let data = ""
+            let hasError = false
+            return https.call<RemoteData | null>(
+                'registry.npmjs.org',
+                `/${key}`,
+                (d) => {
+                    data += d
+                },
+                (err) => {
+                    console.error('https', err)
+                },
+                () => {
+                    if (hasError) {
+                        return asyncLib.value(null)
+                    }
+                    const json = JSON.parse(data)
+                    if (json["dist-tags"] === undefined || json["dist-tags"].latest === undefined) {
+                        return asyncLib.value({
+                            latestVersion: null,
+                            contentFingerprint: null,
+                        })
+                    } else {
+                        const latest = json["dist-tags"].latest
+                        if (json["versions"][latest] === undefined || json["versions"][latest]["content-fingerprint"] === undefined) {
+                            return asyncLib.value({
+                                latestVersion: latest,
+                                contentFingerprint: null,
+                            })
+                        } else {
+                            return asyncLib.value({
+                                latestVersion: latest,
+                                contentFingerprint: json["versions"][latest]["content-fingerprint"],
+
+                            })
+                        }
+                    }
+                }
+
+            )
         }
     )
 
-    asyncLib.rewrite<Overview, asyncAPI.IDictionary<Project>>(
-        fsLib.createCompositeDirReader<Project>(
+    return asyncLib.rewrite<Overview, asyncAPI.IDictionary<Project>>(
+        fsLib.directory<Project>(
             rootDir,
             (projectDir) => {
                 return asyncLib.rewrite<Project, asyncAPI.Tuple2<boolean, asyncAPI.IDictionary<Part>>>(
-                    asyncLib.create2Tuple<boolean, asyncAPI.IDictionary<Part>>(
+                    asyncLib.tuple2<boolean, asyncAPI.IDictionary<Part>>(
                         createLeafProcessCall<boolean>(
                             `git -C ${projectDir.path} diff --exit-code && git -C ${projectDir.path} log origin/master..master --exit-code`,
                             (cleanstdout) => {
@@ -111,7 +102,7 @@ export function getData(
                                 return false
                             },
                         ),
-                        fsLib.createCompositeDirReader<Part>(
+                        fsLib.directory<Part>(
                             projectDir.path,
                             (partDir) => {
                                 if ([
@@ -124,15 +115,14 @@ export function getData(
                                 ].indexOf(partDir.name) === -1) {
                                     return null
                                 } else {
-                                    return fsLib.createCompositeFileReader<Part>(
+                                    return fsLib.file<Part>(
                                         [partDir.path, `package.json`],
                                         (data) => {
                                             const pkg = JSON.parse(data)
 
-                                            function resolveDependencies(source: any) {
-                                                return asyncLib.createDictionaryMapper<string, Depencency>(
-                                                    asyncLib.createDictionary<string>(source === undefined ? {} : source),
-                                                    (v, k) => {
+                                            function resolveDependencies(rawJSONDependencies: any) {
+                                                return asyncLib.dictionary<Depencency>(
+                                                    asyncLib.createDictionary<string>(rawJSONDependencies === undefined ? {} : rawJSONDependencies).map((v, k) => {
                                                         return asyncLib.rewrite(
                                                             registryCache.getEntry(k),
                                                             ($) => {
@@ -143,16 +133,16 @@ export function getData(
                                                             }
                                                         )
 
-                                                    }
+                                                    })
                                                 )
                                             }
 
                                             return asyncLib.rewrite(
-                                                asyncLib.create3Tuple<asyncAPI.IDictionary<Depencency>, asyncAPI.IDictionary<Depencency>, RemoteData | null>(
+                                                asyncLib.tuple3<asyncAPI.IDictionary<Depencency>, asyncAPI.IDictionary<Depencency>, RemoteData | null>(
                                                     resolveDependencies(pkg.dependencies),
                                                     resolveDependencies(pkg.devDependencies),
                                                     pkg.name === undefined
-                                                        ? asyncLib.createLeafSyncCaller(null)
+                                                        ? asyncLib.value(null)
                                                         : registryCache.getEntry(pkg.name)
 
                                                 ),
@@ -172,7 +162,7 @@ export function getData(
                                             )
                                         },
                                         () => {
-                                            return asyncLib.createLeafSyncCaller({
+                                            return asyncLib.value({
                                                 isPublic: ["api", "lib", "bin"].indexOf(partDir.name) !== 1,
                                                 packageData: null
                                             })
@@ -198,5 +188,5 @@ export function getData(
                 projects: projects
             }
         }
-    ).execute(callback)
+    )
 }
